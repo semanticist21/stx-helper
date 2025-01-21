b763b05f73ef3891197f7866e20774b711c1873e
jiwon <semanticist0@gmail.com>
2025-01-21 23:31:34 +0900

    wip

--- index.ts
+++ index.ts
@@ -2,17 +2,14 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 
 export function useStx() {
-    const searchParams = useSearchParams();
-    const router = useRouter();
+	const searchParams = useSearchParams();
+	const router = useRouter();
 
-    const add = (params: Record<string, unknown>) => {
-    }
+	const add = (params: Record<string, unknown>) => {};
 
-    const remove = (params: Record<string, unknown>) => {
-    }
+	const remove = (params: Record<string, unknown>) => {};
 
-    const get = (params: Record<string, unknown>) => {
-    }
+	const get = (params: Record<string, unknown>) => {};
 
-    return [searchParams, { add, remove, get }]
+	return [searchParams, { add, remove, get }];
 }
