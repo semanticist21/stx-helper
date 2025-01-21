da8fc91befcfe26f2e898284630baed574ca8105
jiwon <semanticist0@gmail.com>
2025-01-21 23:28:54 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,7 +1,18 @@
-export interface StxOptions {
-    url: string;
-}
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
-export function stx(options: StxOptions) {
-    console.log(options.url);
+export function useStx() {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const add = (params: Record<string, unknown>) => {
+    }
+
+    const remove = (params: Record<string, unknown>) => {
+    }
+
+    const get = (params: Record<string, unknown>) => {
+    }
+
+    return [searchParams, { add, remove, get }]
 }
