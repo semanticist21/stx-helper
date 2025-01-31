b0cc4e7b3cb6e316bfceba51ea859576ebabe635
jiwon <semanticist0@gmail.com>
2025-01-31 15:07:44 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,18 +1,35 @@
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import {usePathname, useSearchParams} from "next/navigation";
+import {useRouter} from "next/router";
+import queryString from 'query-string';
 
 export function useStx() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
 
-  const stx = {};
+	const router = useRouter();
 
-  const setStx = {
-    add: () => {},
-    remove: () => {},
-    get: () => {},
-  };
+	const ctx = {
+		href: (url?: string, options? : {
+			query: queryString.StringifyOptions
+		}) => {
+			const _url = url ?? pathname;
+			const params = Object.fromEntries(searchParams.entries());
 
-  return [stx, setStx];
+			return `${_url}?${queryString.stringify(params)}`;
+		},
+		push: (url?: string) => {
+			const _url = url ?? pathname;
+			const params = Object.fromEntries(searchParams.entries());
+
+			router.push(_url);
+		},
+		record: () => {},
+	};
+
+	const setCtx = {
+		add: (searchParams: Record<string, unknown>) => {},
+		remove: (searchParams: Record<string, unknown>) => {},
+	};
+
+	return [ctx, setCtx];
 }
