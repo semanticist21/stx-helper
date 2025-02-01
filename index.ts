959eb341d4670eee2e87c87ee1ae00daf4619edd
박지원 (웹개발프론티어TF) <jiwon.park14@kt.com>
2025-02-01 23:29:46 +0900

    wip

--- index.ts
+++ index.ts
@@ -1,39 +1,60 @@
 import qs from "query-string";
 import { useEffect, useState } from "react";
 
-type SearchParam<T> = Record<string, T>;
+type Options = {
+  disableReplace?: boolean;
+};
 
-export function useStx<T = unknown>() {
-  const [search, setSearch] = useState<SearchParam<T>>({});
+export function useStx(qsOptions?: qs.StringifyOptions) {
+  const [search, setSearch] = useState<Record<string, unknown>>({});
 
   useEffect(() => {
     const searchParams = qs.parse(window.location.search);
-    setSearch(searchParams as SearchParam<T>);
+    setSearch(searchParams);
   }, []);
 
   return {
     helper: {
-      add: (record: Record<string, boolean>) => {
+      add: (record: Record<string, boolean>, options?: Options) => {
         const newParams = Object.assign({ ...search }, record);
         setSearch(newParams);
+
+        if (options?.disableReplace) {
+          return;
+        }
+
+        const pathname = window.location.pathname;
+        window.location.replace(
+          `${pathname}?${qs.stringify(search, qsOptions)}`,
+        );
       },
-      remove: (...keys: string[]) => {
+      remove: (keys: Record<string, boolean>, options?: Options) => {
         const newParams = { ...search };
 
-        for (const key of keys) {
-          delete newParams[key];
+        for (const [key, value] of Object.entries(keys)) {
+          if (value) delete newParams[key];
         }
 
         setSearch(newParams);
+
+        if (options?.disableReplace) {
+          return;
+        }
+
+        const pathname = window.location.pathname;
+        window.location.replace(
+          `${pathname}?${qs.stringify(search, qsOptions)}`,
+        );
       },
     },
 
     stx: {
-      link: (url?: string, options?: qs.StringifyOptions) => {
+      link: (url?: string) => {
         const _url = url ?? window.location.pathname;
 
-        return `${_url}?${qs.stringify(search, options)}`;
+        return `${_url}?${qs.stringify(search, qsOptions)}`;
       },
+      params: () => search,
     },
   };
 }
