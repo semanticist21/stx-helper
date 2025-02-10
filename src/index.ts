f226099febafef409b5789b8fd0f39154c219799
jiwon <semanticist0@gmail.com>
2025-02-10 21:19:47 +0900

    feat: add vitest, format files

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,66 @@
 import qs from "query-string";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 type Options = {
-  disableReplace?: boolean;
+	disableReplace?: boolean;
 };
 
 export function useStx(qsOptions?: qs.StringifyOptions) {
-  const [search, setSearch] = useState<Record<string, unknown>>({});
+	const [search, setSearch] = useState<Record<string, unknown>>({});
 
-  useEffect(() => {
-    const searchParams = qs.parse(window.location.search);
-    setSearch(searchParams);
-  }, []);
+	useEffect(() => {
+		const searchParams = qs.parse(window.location.search);
+		setSearch(searchParams);
+	}, []);
 
-  return {
-    helper: {
-      add: (record: Record<string, boolean>, options?: Options) => {
-        const newParams = Object.assign({ ...search }, record);
-        setSearch(newParams);
+	return {
+		helper: {
+			add: (record: Record<string, boolean>, options?: Options) => {
+				const newParams = Object.assign({...search}, record);
+				setSearch(newParams);
 
-        if (options?.disableReplace) {
-          return;
-        }
+				if (options?.disableReplace) {
+					return;
+				}
 
-        const pathname = window.location.pathname;
-        window.location.replace(
-          `${pathname}?${qs.stringify(search, qsOptions)}`,
-        );
-      },
-      remove: (keys: Record<string, boolean>, options?: Options) => {
-        const newParams = { ...search };
+				const pathname = window.location.pathname;
+				window.location.replace(
+					`${pathname}?${qs.stringify(search, qsOptions)}`,
+				);
+			},
+			remove: (keys: Record<string, boolean>, options?: Options) => {
+				const newParams = {...search};
 
-        for (const [key, value] of Object.entries(keys)) {
-          if (value) delete newParams[key];
-        }
+				for (const [key, value] of Object.entries(keys)) {
+					if (value) delete newParams[key];
+				}
 
-        setSearch(newParams);
+				setSearch(newParams);
 
-        if (options?.disableReplace) {
-          return;
-        }
+				if (options?.disableReplace) {
+					return;
+				}
 
-        const pathname = window.location.pathname;
-        window.location.replace(
-          `${pathname}?${qs.stringify(search, qsOptions)}`,
-        );
-      },
-      clear: () => {
-        setSearch({});
+				const pathname = window.location.pathname;
+				window.location.replace(
+					`${pathname}?${qs.stringify(search, qsOptions)}`,
+				);
+			},
+			clear: () => {
+				setSearch({});
 
-        const pathname = window.location.pathname;
-        window.location.replace(pathname);
-      },
-    },
+				const pathname = window.location.pathname;
+				window.location.replace(pathname);
+			},
+		},
 
-    stx: {
-      link: (url?: string) => {
-        const _url = url ?? window.location.pathname;
+		stx: {
+			link: (url?: string) => {
+				const _url = url ?? window.location.pathname;
 
-        return `${_url}?${qs.stringify(search, qsOptions)}`;
-      },
-      params: search,
-    },
-  };
+				return `${_url}?${qs.stringify(search, qsOptions)}`;
+			},
+			params: search,
+		},
+	};
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
 // vite.config.ts
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 import typescript from "@rollup/plugin-typescript";
 import path from "node:path";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
+import {typescriptPaths} from "rollup-plugin-typescript-paths";
 
 const __dirname = path.resolve();
 
 export default defineConfig({
-  plugins: [typescript()],
-  resolve: {},
-  build: {
-    manifest: true,
-    minify: false,
-    reportCompressedSize: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      fileName: "index",
-      formats: ["es", "cjs"],
-    },
-    rollupOptions: {
-      external: [],
-      plugins: [
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        typescript({
-          sourceMap: true,
-          declaration: true,
-          outDir: "dist",
-        }),
-      ],
-    },
-  },
+	plugins: [typescript()],
+	resolve: {},
+	build: {
+		manifest: true,
+		minify: false,
+		reportCompressedSize: true,
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			fileName: "index",
+			formats: ["es", "cjs"],
+		},
+		rollupOptions: {
+			external: [],
+			plugins: [
+				typescriptPaths({
+					preserveExtensions: true,
+				}),
+				typescript({
+					sourceMap: true,
+					declaration: true,
+					outDir: "dist",
+				}),
+			],
+		},
+	},
 });
