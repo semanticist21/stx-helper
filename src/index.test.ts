b87801a28769e713b35b679ded930e4eb7ec3ca8
박지원 (웹개발프론티어TF) <jiwon.park14@kt.com>
2025-02-16 22:43:55 +0900

    wip: test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,31 @@
-import {beforeEach, describe, expect, test} from "vitest";
-import {renderHook} from "@testing-library/react";
+import { beforeEach, describe, expect, test } from "vitest";
+import { act, renderHook } from "@testing-library/react";
+import { useStx } from ".";
 // import {useStx} from ".";
 
-describe("test", () => {
-	beforeEach(() => {
-		window.history.replaceState({}, "", "/test?foo=bar&bar=123");
-	});
+describe("tests", () => {
+  beforeEach(() => {
+    window.history.replaceState({}, "", "/test?foo=bar&bar=123");
+  });
 
-	test("Check an initial value", () => {
-		// const {
-		// 	result: {current},
-		// } = renderHook(() => useStx());
-		// expect(current.stx.params).toEqual({foo: "bar", bar: "123"});
-	});
+  test("Check an initial value", () => {
+    const {
+      result: { current },
+    } = renderHook(() => useStx());
+    expect(current.stx.params).toEqual({ foo: "bar", bar: "123" });
+  });
+
+  test("Check 'Add' function", async () => {
+    const {
+      result: { current },
+    } = renderHook(() => useStx());
+
+    act(() => {
+      current.helper.add({
+        baz: true,
+      });
+    });
+
+    expect(window.location.search).toEqual("foo=bar&bar123&bar=true");
+  });
 });
